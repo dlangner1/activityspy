2d80a55ffcf6cc3ffe57a51d7c0f0c409ed527f4
Dustin Langner <dustin.langner1@gmail.com>
2019-04-17 12:42:41 -0700

    Add application lifecycle methods

--- app/src/main/java/edu/washington/dlangner/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/dlangner/activityspy/MainActivity.kt
@@ -2,11 +2,53 @@ package edu.washington.dlangner.activityspy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "ActivitySpy"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i(TAG, "onCreate event fired")
+
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.i(TAG, "onRestart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i(TAG, "onResume event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.e(TAG, "We're going down Captain!")
     }
 }
